2947e356cef252661464e8c1972274d36c1fed5f
walterlv <xblero@gmail.com>
2025-04-14 16:52:54 +0800

    添加第一批辅助方法

---
+++ src/dotnetCampus.CodeAnalysisUtils/CodeAnalysis/TemplateRegexes.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DotNetCampus.CodeAnalysis.Utils.CodeAnalysis;
+
+/// <summary>
+/// 为通过模板生成的源代码提供正则表达式。
+/// </summary>
+public static class TemplateRegexes
+{
+    private static Regex? _flagRegex;
+    private static Regex? _flag1Regex;
+    private static Regex? _flag2Regex;
+    private static Regex? _flag3Regex;
+    private static Regex? _flag4Regex;
+
+    private static Regex FlagRegex => _flagRegex ??= GetFlagRegex();
+    private static Regex Flag1Regex => _flag1Regex ??= GetFlag1Regex();
+    private static Regex Flag2Regex => _flag2Regex ??= GetFlag2Regex();
+    private static Regex Flag3Regex => _flag3Regex ??= GetFlag3Regex();
+    private static Regex Flag4Regex => _flag4Regex ??= GetFlag4Regex();
+
+    private static Regex GetFlagRegex() => _flagRegex ??= new Regex(@"(?<=\n)\s+// <FLAG>.+?</FLAG>", RegexOptions.Compiled | RegexOptions.Singleline);
+    private static Regex GetFlag1Regex() => _flag2Regex ??= new Regex(@"(?<=\n)\s+// <FLAG1>.+?</FLAG1>", RegexOptions.Compiled | RegexOptions.Singleline);
+    private static Regex GetFlag2Regex() => _flag2Regex ??= new Regex(@"(?<=\n)\s+// <FLAG2>.+?</FLAG2>", RegexOptions.Compiled | RegexOptions.Singleline);
+    private static Regex GetFlag3Regex() => _flag3Regex ??= new Regex(@"(?<=\n)\s+// <FLAG3>.+?</FLAG3>", RegexOptions.Compiled | RegexOptions.Singleline);
+    private static Regex GetFlag4Regex() => _flag3Regex ??= new Regex(@"(?<=\n)\s+// <FLAG4>.+?</FLAG4>", RegexOptions.Compiled | RegexOptions.Singleline);
+
+    /// <summary>
+    /// 替换代码中的 // <FLAG>...</FLAG> 注释，将其替换为指定的内容。
+    /// </summary>
+    /// <param name="content">包含要替换的代码的字符串。</param>
+    /// <param name="flagContent">要替换的内容。</param>
+    /// <returns>替换后的字符串。</returns>
+    public static string FlagReplace(this string content, string flagContent)
+    {
+        return FlagRegex.Replace(content, flagContent);
+    }
+
+    /// <summary>
+    /// 替换代码中的 // <FLAG1>...</FLAG1> 注释，将其替换为指定的内容。
+    /// </summary>
+    /// <param name="content">包含要替换的代码的字符串。</param>
+    /// <param name="flagContent">要替换的内容。</param>
+    /// <returns>替换后的字符串。</returns>
+    public static string Flag1Replace(this string content, string flagContent)
+    {
+        return Flag1Regex.Replace(content, flagContent);
+    }
+
+    /// <summary>
+    /// 替换代码中的 // <FLAG2>...</FLAG2> 注释，将其替换为指定的内容。
+    /// </summary>
+    /// <param name="content">包含要替换的代码的字符串。</param>
+    /// <param name="flagContent">要替换的内容。</param>
+    /// <returns>替换后的字符串。</returns>
+    public static string Flag2Replace(this string content, string flagContent)
+    {
+        return Flag2Regex.Replace(content, flagContent);
+    }
+
+    /// <summary>
+    /// 替换代码中的 // <FLAG3>...</FLAG3> 注释，将其替换为指定的内容。
+    /// </summary>
+    /// <param name="content">包含要替换的代码的字符串。</param>
+    /// <param name="flagContent">要替换的内容。</param>
+    /// <returns>替换后的字符串。</returns>
+    public static string Flag3Replace(this string content, string flagContent)
+    {
+        return Flag3Regex.Replace(content, flagContent);
+    }
+
+    /// <summary>
+    /// 替换代码中的 // <FLAG4>...</FLAG4> 注释，将其替换为指定的内容。
+    /// </summary>
+    /// <param name="content">包含要替换的代码的字符串。</param>
+    /// <param name="flagContent">要替换的内容。</param>
+    /// <returns>替换后的字符串。</returns>
+    public static string Flag4Replace(this string content, string flagContent)
+    {
+        return Flag4Regex.Replace(content, flagContent);
+    }
+}
---
+++ src/dotnetCampus.CodeAnalysisUtils/GeneratorTools.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace DotNetCampus.CodeAnalysis.Utils;
+
+/// <summary>
+/// 提供源生成器相关的杂项工具方法。
+/// </summary>
+internal static class GeneratorTools
+{
+    private static readonly SymbolDisplayFormat GlobalDisplayFormat = new SymbolDisplayFormat(
+        globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.Included,
+        typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
+        genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters,
+        miscellaneousOptions:
+        SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers |
+        SymbolDisplayMiscellaneousOptions.IncludeNullableReferenceTypeModifier |
+        SymbolDisplayMiscellaneousOptions.UseSpecialTypes);
+
+    /// <summary>
+    /// 将符号转换为适合源生成器输出的完整类型名称。<br/>
+    /// 例如：
+    /// <code>
+    /// string
+    /// int?
+    /// global::DotNetCampus.Namespace.TypeName
+    /// global::DotNetCampus.Namespace.TypeName?
+    /// global::DotNetCampus.Namespace.TypeName&lt;string, global::System.Action?&gt;
+    /// </code>
+    /// </summary>
+    /// <param name="symbol">要转换为完整名称的符号。</param>
+    /// <returns>类型的完整名称。</returns>
+    public static string ToGlobalCodeString(this ISymbol symbol)
+    {
+        return symbol.ToDisplayString(GlobalDisplayFormat);
+    }
+}
