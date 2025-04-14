585e4dee1a3f66431c149c135817f64a027bd121
walterlv <xblero@gmail.com>
2025-04-14 17:26:10 +0800

    整理嵌入的源

---
+++ src/dotnetCampus.CodeAnalysisUtils/Generators/EmbeddedGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using DotNetCampus.CodeAnalysis.Utils.CodeAnalysis;
+using DotNetCampus.CodeAnalysis.Utils.IO;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Text;
+
+namespace DotNetCampus.CodeAnalysis.Utils.Generators;
+
+/// <summary>
+/// 将模板中的源代码生成到目标项目中。
+/// </summary>
+[Generator]
+public class EmbeddedGenerator : IIncrementalGenerator
+{
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterSourceOutput(context.AnalyzerConfigOptionsProvider, Execute);
+    }
+
+    private void Execute(SourceProductionContext context, AnalyzerConfigOptionsProvider analyzerConfigOptionsProvider)
+    {
+        if (analyzerConfigOptionsProvider.GlobalOptions.TryGetValueWithFallback("RootNamespace", "MSBuildProjectName") is not { } rootNamespace)
+        {
+            return;
+        }
+
+        foreach (var source in EmbeddedSourceFiles.Enumerate(null))
+        {
+            context.AddSource(source.FileRelativePath.Replace("/", "."), SourceText.From(source.Content, Encoding.UTF8));
+        }
+    }
+}
